58f52a76b270176e2eb69e184272a7697ebd3772
maxcian <max.cian@orientalverse.com>
2021-06-10 12:29:07 +0800

    test(concurrent queue): added

--- pkg/concurrentqueue/concurrentqueue.go
+++ pkg/concurrentqueue/concurrentqueue.go
@@ -13,6 +13,7 @@ type Queue interface {
 	Enqueue(id string)
 	BatchEnqueue(ids []string)
 	Dequeue() (string, error)
+	Len() int
 }
 
 func New() Queue {
@@ -41,12 +42,20 @@ func (c *filo) BatchEnqueue(ids []string) {
 func (c *filo) Dequeue() (string, error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+
 	len := len(c.queue)
 	if len == 0 {
 		return "", ErrEmpty
 	}
 
 	ret := c.queue[len-1]
-	c.queue = c.queue[0 : len-1]
+	c.queue = c.queue[:len-1]
 	return ret, nil
+}
+
+func (c *filo) Len() int {
+	c.mu.RLock()
+	len := len(c.queue)
+	c.mu.RUnlock()
+	return len
 }
--- pkg/concurrentqueue/concurrentqueue_test.go
+++ pkg/concurrentqueue/concurrentqueue_test.go
@@ -1,1 +1,84 @@
 package concurrentqueue
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Enqueue(t *testing.T) {
+	queue := New()
+	numG := 100000
+	var wg sync.WaitGroup
+	wg.Add(numG)
+	for i := 0; i < numG; i++ {
+		go func(i int) {
+			defer wg.Done()
+			queue.Enqueue(fmt.Sprintln(i))
+		}(i)
+	}
+	wg.Wait()
+	assert.Equal(t, numG, queue.Len())
+}
+
+func Test_BatchEnqueue(t *testing.T) {
+	queue := New()
+	factor := 2
+	numG := 100000
+	var wg sync.WaitGroup
+	wg.Add(numG)
+	for i := 0; i < numG; i++ {
+		go func(i int) {
+			defer wg.Done()
+			ids := make([]string, 0, factor)
+			for j := 0; j < factor; j++ {
+				ids = append(ids, fmt.Sprintln(j))
+			}
+			queue.BatchEnqueue(ids)
+		}(i)
+	}
+	wg.Wait()
+	assert.Equal(t, numG*factor, queue.Len())
+}
+
+func Test_Dequeue(t *testing.T) {
+	t.Run("empty should return err", func(t *testing.T) {
+		queue := New()
+		ele, err := queue.Dequeue()
+		assert.Error(t, err)
+		assert.Empty(t, ele)
+	})
+	t.Run("empty should return err", func(t *testing.T) {
+		queue := New()
+		numG := 100000
+		for i := 0; i < numG; i++ {
+			queue.Enqueue(fmt.Sprintln(i))
+		}
+
+		numDecrease := numG / 2
+		errCollect := make(chan error, numDecrease)
+		var wg sync.WaitGroup
+		wg.Add(numDecrease)
+		for i := 0; i < numDecrease; i++ {
+			go func(i int) {
+				defer wg.Done()
+				_, err := queue.Dequeue()
+				errCollect <- err
+			}(i)
+		}
+		wg.Wait()
+		close(errCollect)
+
+		countErr := 0
+		for err := range errCollect {
+			if err != nil {
+				countErr++
+			}
+		}
+		assert.Equal(t, 0, countErr)
+		assert.Equal(t, numG-numDecrease, queue.Len())
+	})
+
+}
