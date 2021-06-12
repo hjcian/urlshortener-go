391626e5ba310850057a0c26c5079ce31e865bd1
maxcian <max.cian@orientalverse.com>
2021-06-12 23:05:57 +0800

    test(cache): refactor test cases using testify/suite

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -7,7 +7,7 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"go.uber.org/zap"
 )
 
@@ -33,56 +33,61 @@ func (d *dbRecorder) Delete(ctx context.Context, id string) error {
 	return nil
 }
 
-func Test_Cache_Get(t *testing.T) {
-	t.Run("same id should only one goroutine can go to database", func(t *testing.T) {
-		r := dbRecorder{}
-		cache := New(&r, zap.NewNop())
-		ctx := context.Background()
+type cacheTestSuite struct {
+	suite.Suite
+	dbRecorder dbRecorder
+	cache      repository.Repository
+	ctx        context.Context
+	numG       int
+}
 
-		numG := 200000
+func (suit *cacheTestSuite) SetupTest() {
+	suit.dbRecorder = dbRecorder{}
+	suit.cache = New(&suit.dbRecorder, zap.NewNop())
+	suit.ctx = context.Background()
+	suit.numG = 200000
+}
+
+func (suit *cacheTestSuite) Test_Get_only_one_goroutine_can_hit_database_if_they_query_same_id() {
+
+	var wg sync.WaitGroup
+	wg.Add(suit.numG)
+	for i := 0; i < suit.numG; i++ {
+		go func() {
+			defer wg.Done()
+			suit.cache.Get(suit.ctx, "aaaaaa")
+		}()
+	}
+	wg.Wait()
+
+	suit.Equal(1, suit.dbRecorder.getCount)
+}
+
+func (suit *cacheTestSuite) Test_Get_every_goroutine_is_able_to_hit_database_once_then_hit_cache_while_next_call() {
+	concurrentCall := func(numG int) {
 		var wg sync.WaitGroup
 		wg.Add(numG)
 		for i := 0; i < numG; i++ {
-			go func() {
+			go func(i int) {
 				defer wg.Done()
-				cache.Get(ctx, "aaaaaa")
-			}()
+				suit.cache.Get(suit.ctx, fmt.Sprintln(i))
+			}(i)
 		}
 		wg.Wait()
-
-		assert.Equal(t, 1, r.getCount)
-	})
-	t.Run("diffrent id will go to database only once and hit cache afterwards", func(t *testing.T) {
-		r := dbRecorder{}
-		cache := New(&r, zap.NewNop())
-		ctx := context.Background()
-
-		concurrentCall := func(numG int) {
-			var wg sync.WaitGroup
-			wg.Add(numG)
-			for i := 0; i < numG; i++ {
-				go func(i int) {
-					defer wg.Done()
-					cache.Get(ctx, fmt.Sprintln(i))
-				}(i)
-			}
-			wg.Wait()
-		}
-		numG := 100000
-		concurrentCall(numG) // first call
-		assert.Equal(t, numG, r.getCount)
-		concurrentCall(numG) // second call
-		assert.Equal(t, numG, r.getCount)
-	})
+	}
+	concurrentCall(suit.numG) // first call
+	suit.Equal(suit.numG, suit.dbRecorder.getCount, "hit database")
+	concurrentCall(suit.numG) // second call
+	suit.Equal(suit.numG, suit.dbRecorder.getCount, "hit cache, so `getCount` does not increse")
 }
 
-func Test_Cache_Delete(t *testing.T) {
-	t.Run("should hit database", func(t *testing.T) {
-		r := dbRecorder{}
-		cache := New(&r, zap.NewNop())
+func (suit *cacheTestSuite) Test_Delete_should_hit_database() {
+	err := suit.cache.Delete(suit.ctx, "aaaaaa")
+	suit.NoError(err)
+	suit.Equal(1, suit.dbRecorder.deleteCount)
+	// TODO: use bloom filter to avoid non-existent record has chance to hit database
+}
 
-		err := cache.Delete(context.Background(), "aaaaaa")
-		assert.NoError(t, err)
-		assert.Equal(t, 1, r.deleteCount)
-	})
+func Test_cacheTestSuite(t *testing.T) {
+	suite.Run(t, new(cacheTestSuite))
 }
--- idgenerator/idgenerator_test.go
+++ idgenerator/idgenerator_test.go
@@ -13,9 +13,6 @@ import (
 	"go.uber.org/zap"
 )
 
-// UPDATE "urls" SET "deleted_at"=NULL,"expired_at"='2021-08-09 09:20:41',"url"='https://example123.com',"updated_at"='2021-06-10 10:33:37.669' WHERE id = 'A4fNMF'
-// SELECT "id" FROM "urls" WHERE deleted_at IS NOT NULL OR expired_at < '2021-06-10 10:27:54.923'
-
 type recorder struct {
 	repository.Repository
 	wg          *sync.WaitGroup
